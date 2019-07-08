0cffae75d0b20cf9e057afedb2acd34bf9241fbe
Student <jsitbon@qcc.cuny.edu>
2019-07-08 17:24:18 -0400

    fourth commit

--- src/App.js
+++ src/App.js
@@ -17,15 +17,33 @@ class App extends Component {
       <div className="container">
         <div className="navbar">Selected:&nbsp;&nbsp;&nbsp;&nbsp;<span className="name">{this.state.shape}</span></div>
         <div className="shape-list">
-          <div className="shape square" data-shape="Square" onClick={this.handleClick}></div>
-          <div className="shape circle" data-shape="Circle" onClick={this.handleClick}></div>
-          <div className="shape triangle" data-shape="Triangle" onClick={this.handleClick}></div>
-          <div className="shape oval" data-shape="Oval" onClick={this.handleClick}></div>
-          <div className="shape star" data-shape="Star" onClick={this.handleClick}></div>
-          <div className="shape triangle_down" data-shape="Triangle_down" onClick={this.handleClick}></div>
-          <div className="shape triangle_right" data-shape="Triangle_right" onClick={this.handleClick}></div>
-          <div className="shape triangle_left" data-shape="Triangle_left" onClick={this.handleClick}></div>
-          <div className="shape trapezium" data-shape="Trapezium" onClick={this.handleClick}></div>
+          <div>
+            <div className="shape square" data-shape="Square" onClick={this.handleClick}></div>
+          </div>
+          <div>
+            <div className="shape circle" data-shape="Circle" onClick={this.handleClick}></div>
+          </div>
+          <div>
+            <div className="shape triangle" data-shape="Triangle" onClick={this.handleClick}></div>
+          </div>
+          <div> 
+            <div className="shape oval" data-shape="Oval" onClick={this.handleClick}></div>
+          </div>
+          <div>
+            <div className="shape star" data-shape="Star" onClick={this.handleClick}></div>
+          </div>
+          <div>
+            <div className="shape triangle_down" data-shape="Triangle_down" onClick={this.handleClick}></div>
+          </div>
+          <div>
+            <div className="shape triangle_right" data-shape="Triangle_right" onClick={this.handleClick}></div>
+          </div>
+          <div>
+            <div className="shape triangle_left" data-shape="Triangle_left" onClick={this.handleClick}></div>
+          </div>
+          <div>
+            <div className="shape trapezium" data-shape="Trapezium" onClick={this.handleClick}></div>
+          </div>
         </div>
       </div>
     )
