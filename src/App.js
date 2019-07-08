c7edb425d8653add7d0dd28ef1f368e6011aed5c
Student <jsitbon@qcc.cuny.edu>
2019-07-08 16:45:00 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    shape: ''
+  }
+
+  handleClick = (e) => {
+    this.setState({
+      shape: e.target.dataset.shape
+    })
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <h1>APP!!!!!!!! :)</h1>
+        <h2>Selected: {this.state.shape}</h2>
+        <div className="shapes">
+          <div className="shape square" data-shape="Square" onClick={this.handleClick}></div>
+          <div className="shape circle" data-shape="Circle" onClick={this.handleClick}></div>
+          <div className="shape triangle" data-shape="Triangle" onClick={this.handleClick}></div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
