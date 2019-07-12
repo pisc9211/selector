317523d6b20510edf4e97bbea22b58a6fc8611b1
pisc9211 <pisc9211@gmail.com>
2019-07-12 13:21:50 -0400

    Updates

---
+++ precache-manifest.80473e81e4233b1c31b7401d09af0e3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e35fd272b79631060dd691347eeeb894",
+    "url": "/selector/index.html"
+  },
+  {
+    "revision": "8df4aa2013ebfcb046a2",
+    "url": "/selector/static/css/main.5c3573f5.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/selector/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "8df4aa2013ebfcb046a2",
+    "url": "/selector/static/js/main.76e2131b.chunk.js"
+  },
+  {
+    "revision": "90afcdfb8efb11a1f025",
+    "url": "/selector/static/js/runtime~main.3573b4bb.js"
+  }
+]);
